3ddaeaaf0a890dcaa81fa68f49d15831d564fae4
Andreas Neuhaus <zargony@zargony.com>
2013-03-28 23:16:04 +0100

    Configure and update APT

--- modules/zargony/manifests/base.pp
+++ modules/zargony/manifests/base.pp
@@ -1,4 +1,8 @@
-class zargony::base {
+class zargony::base (
+	$ubuntu_area = 'de',
+	$ubuntu_distribution = 'precise',
+	$ubuntu_components = 'main restricted universe multiverse',
+) {
 
 	# Ensure that the root password is disabled
 	user { 'root':
@@ -16,5 +20,24 @@ class zargony::base {
 		source  => 'puppet:///modules/zargony/authorized_keys',
 		mode    => 0644, owner => 'root', group => 'root',
 		require => File['/root/.ssh'],
+	}
+
+	# Configure and update APT
+	file { '/etc/apt/sources.list':
+		ensure  => present,
+		content => template("zargony/sources.list.erb"),
+		mode    => 0644, owner => 'root', group => 'root',
+		before  => Exec['aptget_update'],
+		notify  => Exec['aptget_update'],
+	}
+	exec { 'aptget_update':
+		command     => '/usr/bin/apt-get -qq update',
+		logoutput   => false,
+		refreshonly => true,
+	}
+
+	# Make sure to update before installing any package
+	Package <| |> {
+		require +> Exec['aptget_update'],
 	}
 }
--- modules/zargony/manifests/server.pp
+++ modules/zargony/manifests/server.pp
@@ -1,3 +1,11 @@
-class zargony::server {
-	class { 'zargony::base': }
+class zargony::server (
+	$ubuntu_area = undef,
+	$ubuntu_distribution = undef,
+	$ubuntu_components = undef,
+) {
+	class { 'zargony::base':
+		ubuntu_area         => $ubuntu_area,
+		ubuntu_distribution => $ubuntu_distribution,
+		ubuntu_components   => $ubuntu_components,
+	}
 }
