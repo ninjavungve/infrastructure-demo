000923e00d32dfbf2a465f0265de4ee090e8c19c
Andreas Neuhaus <zargony@zargony.com>
2013-03-28 21:07:58 +0100

    Install authorized ssh keys

--- modules/zargony/manifests/base.pp
+++ modules/zargony/manifests/base.pp
@@ -1,2 +1,20 @@
 class zargony::base {
+
+	# Ensure that the root password is disabled
+	user { 'root':
+		ensure   => present,
+		password => '*',
+	}
+
+	# Install authorized ssh keys
+	file { '/root/.ssh':
+		ensure => directory,
+		mode   => 0644, owner => 'root', group => 'root',
+	}
+	file { '/root/.ssh/authorized_keys':
+		ensure  => present,
+		source  => 'puppet:///modules/zargony/authorized_keys',
+		mode    => 0644, owner => 'root', group => 'root',
+		require => File['/root/.ssh'],
+	}
 }
