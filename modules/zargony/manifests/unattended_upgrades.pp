3b1da27cd2fe5ca68841d63d5e13cd68e54239d7
Andreas Neuhaus <zargony@zargony.com>
2013-03-29 17:23:03 +0100

    Spacing

--- modules/zargony/manifests/unattended_upgrades.pp
+++ modules/zargony/manifests/unattended_upgrades.pp
@@ -9,14 +9,14 @@ class zargony::unattended_upgrades (
 		ensure => $automatic ? { true => present, default => absent },
 		source => 'puppet:///modules/zargony/apt_20auto-upgrades',
 		mode   => 0644, owner => 'root', group => 'root',
-		before  => Exec['aptget_update'],
-		notify  => Exec['aptget_update'],
+		before => Exec['aptget_update'],
+		notify => Exec['aptget_update'],
 	}
 	file { '/etc/apt/apt.conf.d/50unattended-upgrades':
 		ensure => present,
 		source => 'puppet:///modules/zargony/apt_50unattended-upgrades',
 		mode   => 0644, owner => 'root', group => 'root',
-		before  => Exec['aptget_update'],
-		notify  => Exec['aptget_update'],
+		before => Exec['aptget_update'],
+		notify => Exec['aptget_update'],
 	}
 }
