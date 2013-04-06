37077b0d3d3d13c07b5d248de64bfbdf6c206995
Andreas Neuhaus <zargony@zargony.com>
2013-04-06 11:44:49 +0200

    Added git class

---
+++ modules/zargony/manifests/git.pp
@@ -0,0 +1,17 @@
+class zargony::git {
+	package { 'git':
+		ensure => installed,
+	}
+}
+
+define zargony::git::clone (
+	$url,
+	$ref = 'master',
+) {
+	exec { "${name}/.git":
+		path    => $zargony::base::path,
+		command => "git clone -b ${ref} ${url} ${name}",
+		creates => "${name}/.git",
+		require => Package['git'],
+	}
+}
