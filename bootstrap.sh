da42c8503d6d6c1639c291c61d967387cc3d5cc4
Andreas Neuhaus <zargony@zargony.com>
2013-09-04 15:40:12 +0200

    Added authorized ssh keys to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -65,6 +65,16 @@ EOF
 # Set timezone to Europe/Berlin
 ln -sf /usr/share/zoneinfo/Europe/Berlin ${TARGET}/etc/localtime
 
+# Configure SSH access
+if test -x ${TARGET}/usr/sbin/sshd; then
+	mkdir -p ${TARGET}/root/.ssh
+	cat >${TARGET}/root/.ssh/authorized_keys <<-EOF
+		ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsSt+5Ennalg+GM7+0/37ukXuYR523DEWHTygpuGH1CI3GG0vMKHquG2lUEOKJ2mh4Pt5OBXxNfKxl3mmaPxyUStcMBwS25AQQhyLkFGp2sRFKpZQrEYozJ1galkPwdG4OsdtZXDdeDodsttDjIKchPPOSh0bHoXvIkA+zzWBu9wxZKc4EhQHN2+cI268NT+mZYFCFLcL2Zpr+eBW1OvnQ5MdG9kh4jYBc2kORXR4CzzCEVnkoibLLM7cczV96jugouVGTpDIYValBERWOM2aUFEbyRo3vAlveAfoFrYWFmvOgT2ynq1wHG6AcbsOOeAeCLO8slimmmgExhxtTEOGzQ== zargony@lina
+		ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwFQFH9XgcgJ2l43yr69sAkql39VKOCVSMfv6jH/ml0WyD0FP2GO1mMoW/teCuRbWqWxXr7fF0QhOi60g1e4xE766Rkll9xBmx+ckPuSj3xmKOUpnn/Z/rjFzwAQ452lJtIGySSNUbfxM00usDF0+kc5wMR1ugnR3S3y2loVN38RzVgUVMm7r1qhnttsUi/HeXFTw1j7Gaqbmz5PEyMKXeI9fvml1wAzf14JMvxjHSQBBvwLuvAAGMPbdd136bIpY+Vpvc3+zXyODLoFOuyl5cuAHULg0thrpifmgrtkLdD2TpKJqqio2kMrS7A0L5Hg+OXD3qlYPFaMl9tZb0QgBl zargony@priya
+		ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzz9mVFVI6HLTYxV352Q2/9M6pjaWWHXFDJ3+f7j6LceI596rLL2ITgqhxtfN8S2yHWi/8UsBy0xykwsyS7BRmh76/m0nCG+djrqPidG8uZPasicRZyIz/5LxYsi6i9gw/FGv8oL8MvCqilB/zpAqy0/FQEC9wWysbvbB33+q0eJ4wa2FVGm2KW19WkPOysC4jKrc99FcX2pZld5QhU3f6FocTNH4Baq6opzf65SfV40Rp6mERn3FuVrZivcmGt8K7t6rvidQEPxfnoFuuCxN6ZnXAZIOxbFcjqadjI9XaVFPkrtR/5a5xyyOEGDuzN0crxNI8Wd9gYUcbpgCH+R1l zargony@ios
+	EOF
+fi
+
 # When an archive was given as the destination, create it and remove the temporary build directory
 case "${DESTINATION}" in
 	*.tar.gz)
