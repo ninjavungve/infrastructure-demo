17d00542b9e0f43145406c3d4966e969045dacd0
Andreas Neuhaus <zargony@zargony.com>
2013-09-04 13:16:51 +0200

    Install aufs kernel module and userspace tools if missing

--- install-docker.sh
+++ install-docker.sh
@@ -2,10 +2,12 @@
 
 test -x /usr/bin/curl || apt-get install -qy curl
 
-if ! test -d /etc/apt/sources.list.d/docker.list; then
+if ! test -e /etc/apt/sources.list.d/docker.list; then
 	curl -s http://get.docker.io/gpg |apt-key add -
 	echo "deb http://get.docker.io/ubuntu docker main" >/etc/apt/sources.list.d/docker.list
 	apt-get update -qq
 fi
 
+test -e /lib/modules/`uname -r`/kernel/ubuntu/aufs/aufs.ko || apt-get install linux-image-extra-`uname -r`
+test -x /usr/bin/auchk || apt-get install aufs-tools
 test -x /usr/bin/docker || apt-get install -qy lxc-docker
