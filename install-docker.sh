f9d309060451e4ecc7375c8aad939f147886f3ea
Andreas Neuhaus <zargony@zargony.com>
2013-09-04 13:51:49 +0200

    Install silently

--- install-docker.sh
+++ install-docker.sh
@@ -8,6 +8,6 @@ if ! test -e /etc/apt/sources.list.d/docker.list; then
 	apt-get update -qq
 fi
 
-test -e /lib/modules/`uname -r`/kernel/ubuntu/aufs/aufs.ko || apt-get install linux-image-extra-`uname -r`
-test -x /usr/bin/auchk || apt-get install aufs-tools
+test -e /lib/modules/`uname -r`/kernel/ubuntu/aufs/aufs.ko || apt-get install -qy linux-image-extra-`uname -r`
+test -x /usr/bin/auchk || apt-get install -qy aufs-tools
 test -x /usr/bin/docker || apt-get install -qy lxc-docker
