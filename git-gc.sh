be5601eaad199248630cff6525b552136a7f0e30
Andreas Neuhaus <zargony@zargony.com>
2014-02-14 13:07:28 +0100

    Add (cron) script for git repositories maintenance

---
+++ git-gc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for dir in /srv/repositories/*/*.git; do
+	echo === "Optimizing repository $dir"
+	uid=`stat --printf="%u" $dir`
+	gid=`stat --printf="%g" $dir`
+	sudo -H -u \#$uid -g \#$gid GIT_DIR=$dir git gc
+done
