709b364a40e0c67584f38badca22c1612b73efb1
Andreas Neuhaus <zargony@zargony.com>
2014-02-14 14:58:15 +0100

    Add lxc.network.flags=up before configuring IPv6
    This configuration setting seems to have to be added to the lxc configuration when using IPv6. Otherwise starting the container fails with error "Cannot add ipv6 gateway for eth0 when not bringing up the interface"

--- config.daenerys.mk
+++ config.daenerys.mk
@@ -1,14 +1,18 @@
 MIRROR := hetzner
 
-gitserver_run_opts :=	-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:30/112" \
+gitserver_run_opts :=	-lxc-conf="lxc.network.flags = up" \
+						-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:30/112" \
 						-lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
 						$(gitserver_run_opts)
-mailserver_run_opts :=	-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:50/112" \
+mailserver_run_opts :=	-lxc-conf="lxc.network.flags = up" \
+						-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:50/112" \
 						-lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
 						$(mailserver_run_opts)
-minecraft_run_opts :=	-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:10/112" \
+minecraft_run_opts :=	-lxc-conf="lxc.network.flags = up" \
+						-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:10/112" \
 						-lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
 						$(minecraft_run_opts)
-webserver_run_opts :=	-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:20/112" \
+webserver_run_opts :=	-lxc-conf="lxc.network.flags = up" \
+						-lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:20/112" \
 						-lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
 						$(webserver_run_opts)
