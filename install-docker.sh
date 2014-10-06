746a221a05aaa1310b56ac664dad324ba5dbc4ff
Andreas Neuhaus <zargony@zargony.com>
2014-10-06 20:54:20 +0200

    Prepare commands for installing from https repositories

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+#test -e /usr/lib/apt/methods/https || apt-get install -qy apt-transport-https
+#test -e /etc/ssl/certs/ca-certificates.crt || apt-get install -qy ca-certificates
 
 if ! test -e /etc/apt/sources.list.d/docker.list; then
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
