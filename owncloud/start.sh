8e3d60f9dc1fd0a336105ce9e5a1b9b3fd6c1091
Andreas Neuhaus <zargony@zargony.com>
2014-11-17 21:48:40 +0100

    Use exec to get rid of the shell at the end of the start script

--- owncloud/start.sh
+++ owncloud/start.sh
@@ -3,4 +3,4 @@ echo "Starting php5-fpm"
 /usr/sbin/php5-fpm --daemonize --fpm-config /opt/owncloud/php-fpm.conf
 echo "Starting nginx"
 rm -f /var/www/owncloud/backend.sock
-/usr/sbin/nginx -c /opt/owncloud/nginx.conf
+exec /usr/sbin/nginx -c /opt/owncloud/nginx.conf
