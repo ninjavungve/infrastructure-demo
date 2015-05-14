f3b75ce29017d806ce291a7d216c641bbf043cac
Andreas Neuhaus <zargony@zargony.com>
2015-05-14 10:53:30 +0200

    Move some gitblab directories

--- gitlab/unicorn.rb
+++ gitlab/unicorn.rb
@@ -4,8 +4,8 @@ working_directory "/opt/gitlab"
 listen "/var/www/gitlab/backend.sock", :backlog => 64
 listen "0.0.0.0:8080", :tcp_nopush => true
 timeout 30
-stderr_path "/var/log/gitlab/unicorn.stderr.log"
-stdout_path "/var/log/gitlab/unicorn.stdout.log"
+stderr_path "/var/log/gitlab/gitlab/unicorn.stderr.log"
+stdout_path "/var/log/gitlab/gitlab/unicorn.stdout.log"
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
