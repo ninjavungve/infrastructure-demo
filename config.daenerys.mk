91968981a86f88236ecb65e0fe5e568dbb151480
Andreas Neuhaus <zargony@zargony.com>
2015-05-24 10:09:47 +0000

    Remove IPv6 from minecraft container

--- config.daenerys.mk
+++ config.daenerys.mk
@@ -2,10 +2,6 @@ gitlab_run_opts :=		--lxc-conf="lxc.network.flags = up" \
 						--lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:30/112" \
 						--lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
 						$(gitlab_run_opts)
-minecraft_run_opts :=	--lxc-conf="lxc.network.flags = up" \
-						--lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:10/112" \
-						--lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
-						$(minecraft_run_opts)
 webserver_run_opts :=	--lxc-conf="lxc.network.flags = up" \
 						--lxc-conf="lxc.network.ipv6 = 2a01:4f8:100:546f::4:20/112" \
 						--lxc-conf="lxc.network.ipv6.gateway = 2a01:4f8:100:546f::4:1" \
