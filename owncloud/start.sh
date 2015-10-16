bb01277d93c16d98f1a4f80ff657494a2522060d
Andreas Neuhaus <zargony@zargony.com>
2015-10-16 17:15:31 +0200

    Switch to official owncloud image

--- owncloud/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting php5-fpm"
-/usr/sbin/php5-fpm --daemonize --fpm-config /opt/owncloud/php-fpm.conf
-echo "Starting nginx"
-rm -f /var/www/owncloud/backend.sock
-exec /usr/sbin/nginx -c /opt/owncloud/nginx.conf
