6684eed7ef3088b062118e176ec0df8267e8ce0b
Andreas Neuhaus <zargony@zargony.com>
2016-03-23 12:46:15 +0100

    Use GitLab's rake task for daily git repository maintenance

--- git-gc.sh
+++ git-gc.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-for dir in /var/lib/docker/volumes/infrastructure_gitlab/_data/repositories/*/*.git; do
-	echo "=== Optimizing repository $dir"
-	uid=`stat --printf="%u" $dir`
-	gid=`stat --printf="%g" $dir`
-	sudo -H -u \#$uid -g \#$gid GIT_DIR=$dir git gc
-done
+docker exec -it gitlab bundle exec rake gitlab:git:gc RAILS_ENV=production
