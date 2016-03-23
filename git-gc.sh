6ecb77b5dae28023a46d4f3198ae8d966c63d305
Andreas Neuhaus <zargony@zargony.com>
2016-03-23 12:31:38 +0100

    Update daily git maintenance paths

--- git-gc.sh
+++ git-gc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for dir in /srv/gitlab/repositories/*/*.git; do
+for dir in /var/lib/docker/volumes/infrastructure_gitlab/_data/repositories/*/*.git; do
 	echo "=== Optimizing repository $dir"
 	uid=`stat --printf="%u" $dir`
 	gid=`stat --printf="%g" $dir`
